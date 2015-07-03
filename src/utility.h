967926017981dc6d6b5d37bb8b0fe501718df7b5
wadetang <twzhi0614@vip.qq.com>
2015-07-03 17:36:53 +0800

    header
    header

---
+++ src/utility.h
@@ -0,0 +1,16 @@
+/*
+ * WebServer -- A mini HTTP Server
+ * Copyright (C) 2015 Tangwz. All rights reserved.
+ * 
+ * Author:       Tangwz
+ * File Name:    utility.h
+ * Data:         2015/6/3
+ * Blog:         tangwz.com
+ */
+
+#ifndef _UTILITY_H_
+#define _UTILITY_H_
+
+
+
+#endif
---
+++ src/webserver.h
@@ -0,0 +1,14 @@
+/*
+ * WebServer -- A mini HTTP Server
+ * Copyright (C) 2015 Tangwz. All rights reserved.
+ * 
+ * Author:       Tangwz
+ * File Name:    webserver.h
+ * Data:         2015/6/3
+ * Blog:         tangwz.com
+ */
+
+#ifndef _WEBSERVER_H_
+#define _WEBSERVER_H_
+
+#endif
