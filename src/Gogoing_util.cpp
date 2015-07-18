26f30940ae26b54332dd03a8a11deee18d0219f6
tangwz <tangwz.com@gmail.com>
2015-07-18 14:33:38 +0800

    add memory wrapper function

--- src/Gogoing_util.cpp
+++ src/Gogoing_util.cpp
@@ -10,8 +10,31 @@
 
 #include "Gogoing_util.h"
 
+
+/*
+ * Memeroy wrapper function
+ */
 void *going_calloc(size_t nmemb, size_t size)
 {
 	void *ptr = calloc(nmemb, size);
-	
+	if(NULL == ptr){
+		debug("going_calloc failed.");
+		exit(-1);
+	}
+	return ptr;
+}
+
+void *going_malloc(size_t size)
+{
+	void *ptr = malloc(size);
+	if(NULL == ptr){
+		debug("going_malloc failed.");
+		exit(-1);
+	}
+	return ptr;
+}
+
+void going_free(void *ptr)
+{
+	free(ptr);
 }
