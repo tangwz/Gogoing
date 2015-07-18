e17b471b877fccdfc5425472ab35ec51656176a1
tangwz <tangwz.com@gmail.com>
2015-07-18 14:58:52 +0800

    add epoll wrapper function

--- src/http_epoll.cpp
+++ src/http_epoll.cpp
@@ -13,10 +13,48 @@
 
 struct epoll_event *events;
 
+/*
+ *  epoll_create1() returns a file descriptor referring to the new epoll instance.  
+ *  This file descriptor is used for all the subsequent calls to the epoll interface. 
+ */
 int going_epoll_create(int flags)
 {
+	//
 	int fd = epoll_create1(flags);
-	check(fd > 0, "epoll create");
+	check(fd > 0, "going_epoll create failed.");
 
-	events = (struct epoll_event *)malloc()
+	events = (struct epoll_event *)going_malloc(sizeof(struct epoll_event) * MAXEVENTS);
+	return fd;
+}
+
+/*
+ * @about epoll_ctl: http://man7.org/linux/man-pages/man2/epoll_ctl.2.html
+ * @Synopsis: int epoll_ctl(int epfd, int op, int fd, struct epoll_event *event);
+ */
+void going_epoll_add(int epfd, int fd, struct epoll_event* event)
+{
+	int rc = epoll_ctl(epfd, EPOLL_CTL_ADD, fd, event);
+	check(rc == 0, "going_epoll_add failed.");
+	return;
+}
+
+void going_epoll_mod(int epfd, int fd, struct epoll_event* event)
+{
+	int rc = epoll_ctl(epfd, EPOLL_CTL_MOD, fd, event);
+	check(rc == 0, "going_epoll_mod failed.");
+	return;
+}
+
+void going_epoll_del(int epfd, int fd, struct epoll_event* event)
+{
+	int rc = epoll_ctl(epfd, EPOLL_CTL_DEL, fd, event);
+	check(rc == 0, "going_epoll_del failed.");
+	return;
+}
+
+int going_epoll_wait(int epfd, struct epoll_event *event, int maxevents, int timeout)
+{
+	int n = epoll_wait(epfd, events, maxevents, timeout);
+	check(n > 0, "going_epoll_wait failed.");
+	return n;
 }
