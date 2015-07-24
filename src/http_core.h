53ca4291add22d86312f9538b57cfc02860b17dc
tangwz <tangwz.com@gmail.com>
2015-07-24 23:38:54 +0800

    add thread func

--- src/http_core.h
+++ src/http_core.h
@@ -11,4 +11,38 @@
 #ifndef _HTTP_CORE_H_
 #define _HTTP_CORE_H_
 
+/*
+ * @brief: Handle client's thread.
+ * @param param: Client's conn_sock.
+ * @return: None.
+ */
+void *going_thread_func(void *param);
+
+/*
+ * @brief: Number of threads.
+ */
+int32_t going_thread_num = 0;
+pthread_mutex_t going_thread_num_mutex = PTHREAD_MUTEX_INITIALIZER;
+
+/*
+ * @brief: going_thread_num add(Atomicity) 1.
+ * @param: None.
+ * @return: None.
+ */
+void going_thread_num_add();
+
+/*
+ * @brief: going_thread_num minus(Atomicity) 1.
+ * @param: None.
+ * @return: None.
+ */
+void going_thread_num_minus();
+
+/*
+ * @brief: Read going_thread_num(Atomicity).
+ * @param: None.
+ * @return: The value of going_thread_num.
+ */
+int32_t going_thread_num_read();
+
 #endif
