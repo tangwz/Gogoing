f73d6359ee1791009339b11ff406aa1db34da121
tangwz <tangwz.com@gmail.com>
2015-07-26 17:14:34 +0800

    add main function.

--- src/http_core.cpp
+++ src/http_core.cpp
@@ -12,7 +12,111 @@
 
 
 /* set timeout.(milliseconds) */
- #define TIMEOUT 1000*60*4
+#define TIMEOUT 1000*60*4
+
+/******* 函数声明  *******/
+void *going_thread_func(void *param);
+
+/*
+ * @brief: Number of threads.
+ */
+int32_t going_thread_num = 0;
+pthread_mutex_t going_thread_num_mutex = PTHREAD_MUTEX_INITIALIZER;
+
+void going_thread_num_add();
+void going_thread_num_minus();
+int32_t going_thread_num_read();
+int going_do_http_header(going_http_header_t *phttpdr, string& out);
+
+/******* web服务器程序入口函数 *******/
+int main(int argc, char const *argv[])
+{
+	struct epoll_event ev;
+	struct epoll_event events[MAXEVENTS];
+	struct sockaddr_in server_addr;
+	struct sockaddr_in client_addr;
+	socklen_t       addrlen;
+	pthread_attr_t  pthread_attr_detach;
+	_epollfd_connfd epollfd_connfd;
+	pthread_t       tid;
+
+	if(argc != 2){
+		printf("Usage: %s <config_path>\n", argv[0]);
+		exit(-1);
+	}
+	//Is configure existed?
+	if(-1 == going_is_file_existed(argv[1])){
+		perror("going_is_file_existed.");
+		exit(-1);
+	}
+	//parse configure
+	if(-1 == going_parse_config(argv[1])){
+		printf("going_parse_config error!\n");
+		exit(-1);
+	}
+
+	//创建监听套接字
+	int listen_fd = going_socket(AF_INET, SOCK_STREAM, 0);
+	//设置监听套接字为非阻塞模式
+	going_set_nonblocking(listen_fd);
+	//对套接字设置SO_REUSEADDR选项
+	going_set_reuse_addr(listen_fd);
+	//通过服务名和协议名获得相应的知名端口
+	struct servent* pservent = going_getservbyname("http", "tcp");
+	uint16_t listen_port = pservent->s_port;
+
+	bzero(&server_addr, sizeof(server_addr));
+	server_addr.sin_family = AF_INET;
+	server_addr.sin_port = (listen_port);
+	server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+	going_bind(listen_fd, (struct sockaddr*)&server_addr, sizeof(server_addr));
+	going_listen(listen_fd, MAX_BACKLOG);
+
+	//创建epoll文件描述符
+	int epollfd = going_epoll_create(MAXEVENTS);
+
+	ev.events = EPOLLIN;
+	ev.data.fd = listen_fd;
+	//将监听事件加入epoll中
+	going_epoll_add(epollfd, listen_fd, &ev);
+
+	//设置线程属性为detach
+	pthread_attr_init(&pthread_attr_detach);
+	pthread_attr_setdetachstate(&pthread_attr_detach, PTHREAD_CREATE_DETACHED);
+
+	for(;;){
+		//无限等待直到有描述符就绪
+		int nfds = going_epoll_wait(epollfd, events, MAXEVENTS, -1);
+		//若going_epoll_wait被中断则重新调用该函数
+		if(nfds == -1 && errno == EINTR)
+			continue;
+		for(int n = 0; n != nfds; ++n){
+			if(events[n].data.fd == listen_fd){
+				int conn_sock = going_accept(listen_fd, (struct sockaddr *)&client_addr, addrlen);
+				going_set_nonblocking(conn_sock);
+				ev.events = EPOLLIN | EPOLLET;
+				ev.data.fd = conn_sock;
+
+				going_epoll_add(epollfd, conn_sock, &ev);
+			}else{
+				epollfd_connfd.epollfd = epollfd;
+				epollfd_connfd.connfd = events[n].data.fd;
+				ev.data.fd = conn_sock;
+
+				going_epoll_del(epollfd, conn_sock, &ev);
+
+				pthread_create(&tid, &pthread_attr_detach, &going_thread_func, (void *)&epollfd_connfd);
+			}
+		}
+	}
+
+	pthread_attr_destroy(&pthread_attr_detach);
+
+	close(listen_fd);
+	return 0;
+}
+
 /*
  * @brief: Handle client's thread.
  * @param param: Client's conn_sock.
